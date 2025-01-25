8965baf01919259d770a9e21cd03274d913b3cff
zoherr <workforzoher@gmail.com>
2025-01-25 11:46:14 +0530

    Some change

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Gemini API Tutorial
 
 [Gemni Api Key](https://ai.google.dev/gemini-api/docs?_gl=1*1fumi0o*_ga*Njg0MjU3NjkxLjE3Mzc2Mzg1Mjc.*_ga_P1DBVKWT6V*MTczNzc4NDc0NS40LjAuMTczNzc4NDc0NS42MC4wLjYyNTQ3Nzk5Ng..#node.js)
+
+[Generate text from text-and-image input](https://ai.google.dev/gemini-api/docs/text-generation?lang=node#generate-text-from-text-and-image)
--- server/controllers/analyse.controller.js
+++ server/controllers/analyse.controller.js
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import fs from "fs";
+import * as fs from 'node:fs';
 
 export const analyse = async (req, res, next) => {
     try {
@@ -14,21 +14,22 @@ export const analyse = async (req, res, next) => {
 
         const uploadedImagePath = req.file.path;
 
-        const fsPromises = fs.promises;
-        const base64ImageData = await fsPromises.readFile(uploadedImagePath, { encoding: "base64" });
 
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-        
-        const prompt = "Please examine this plant image and provide a detailed evaluation. Identify the species, assess its health status, and suggest appropriate care guidelines. Include information on its distinguishing features, specific care needs, and any fascinating facts related to the plant. Present the response clearly in plain text without using any formatting or special characters."
-        const result = await model.generateContent([
-            prompt,
-            {
+        function fileToGenerativePart(path, mimeType) {
+            return {
                 inlineData: {
-                    mimeType: req.file.mimetype,
-                    data: base64ImageData,
+                    data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+                    mimeType,
                 },
-            },
-        ]);
+            };
+        }
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+        const prompt = "Please examine this plant image and provide a detailed evaluation. Identify the species, assess its health status, and suggest appropriate care guidelines. Include information on its distinguishing features, specific care needs, and any fascinating facts related to the plant. Present the response clearly in plain text without using any formatting or special characters."
+
+        const imagePart = fileToGenerativePart(uploadedImagePath, "image/png");
+
+        const result = await model.generateContent([prompt, imagePart]);
 
         if (!result) {
             return res.status(400).json({
@@ -37,8 +38,7 @@ export const analyse = async (req, res, next) => {
             });
         }
         const plantAnalysisResult = result.response.text();
-
-        await fsPromises.unlink(uploadedImagePath);
+        await fs.promises.unlink(uploadedImagePath);
 
         res.status(200).json({
             success: true,
